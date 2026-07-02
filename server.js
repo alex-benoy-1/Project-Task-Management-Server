201b60c266c8ce860c8c815fd856cff18ff73678
Alex-Benoy <alexbenoy76@gmail.com>
2026-07-02 20:51:31 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.status(200).json({message: "App succesful"});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
