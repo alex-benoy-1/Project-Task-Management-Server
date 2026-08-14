294d92ac05ffc48736b1ea2fc3e018613b74384e
Alex-Benoy <alexbenoy76@gmail.com>
2026-08-14 23:22:06 +0100

    Adde application logging to organization services

--- src/services/orgMember.service.js
+++ src/services/orgMember.service.js
@@ -1,12 +1,22 @@
 import OrgMemberModel from "../models/orgMember.model.js";
+import logger from "../utils/logger.js";
 
 const removeMember = async (orgId, memberId) => {
     const member = await OrgMemberModel.removeMemberById(orgId, memberId);
 
     if(!member) {
+        logger.warn({ 
+                orgId,
+                memberId: memberId
+            }, "Member not removed from organization"
+        );
         throw new Error("No member found");
     }
-
+    logger.info({ 
+            orgId,
+            memberId: memberId
+        }, "Member removed from organization"
+    );
     return member;
 }
 
@@ -14,8 +24,16 @@ const getMembers = async (orgId) => {
     const members = await OrgMemberModel.getAllMembers(orgId);
 
     if(members.length == 0) {
+        logger.warn({ 
+                orgId,
+            }, "Members details not retrived from organization: No member found"
+        );
         throw new Error("No members found");
     }
+    logger.info({ 
+            orgId,
+        }, "Members details retrived for organization"
+    );
     return members;
 }
 
@@ -23,9 +41,20 @@ const addMember = async (orgId, memberId, role) => {
     const member = await OrgMemberModel.createOrgMember(orgId, memberId, role);
 
     if(!member) {
+        logger.warn({ 
+                orgId,
+                member: memberId,
+                role: role
+            }, "Member not added"
+        );
         throw new Error("Member not added");
     }
-
+    logger.info({ 
+            orgId,
+            member: memberId,
+            role: role
+        }, "Member added"
+    );
     return member;
 }
 
@@ -33,9 +62,20 @@ const changeRole = async (orgId, memberId, role) => {
     const member = await OrgMemberModel.changeRole(orgId, memberId, role);
 
     if(!member) {
+        logger.info({ 
+                orgId,
+                member: memberId,
+                newRole: role
+            }, "Member role not updated"
+        );
         throw new Error("Member role not chnaged");
     }
-
+    logger.info({ 
+            orgId,
+            member: memberId,
+            newRole: role
+        }, "Members role updated"
+    );
     return member;
 }
 
--- src/services/organization.service.js
+++ src/services/organization.service.js
@@ -2,6 +2,7 @@ import OrganizationModel from "../models/organization.model.js";
 import OrgMemberModel from "../models/orgMember.model.js";
 import generateSlug from "../utils/slug.js";
 import pgdb from "../configs/db.config.js";
+import logger from "../utils/logger.js";
 
 
 const createOrganization = async (name, userId) => {
@@ -18,6 +19,14 @@ const createOrganization = async (name, userId) => {
 
         await client.query("COMMIT");
 
+        logger.info(
+            {
+                userId,
+                organizationId: organization.id,
+                organizationName: organization,name
+            },
+            "Organization created successfully"
+        )
         return {
             organization: {
                 id: organization.id,
@@ -31,6 +40,14 @@ const createOrganization = async (name, userId) => {
 
     } catch(err) {
         await client.query("ROLLBACK");
+        logger.error(
+            {
+                userId,
+                organizationName: name,
+                err
+            },
+            "Failed to create organization"
+        );
         throw err;
     } finally {
         client.release();
@@ -39,7 +56,13 @@ const createOrganization = async (name, userId) => {
 
 const getOrganizationByUser = async (userId) => {
     const organizations = await OrganizationModel.getOrganizationsByUser(userId);
-
+    logger.info(
+        {
+            userId,
+        },
+        "Retrived organizations of user"
+    );
+    
     if(organizations.length === 0) {
         return {
             organizations: [],
@@ -57,8 +80,17 @@ const getOrgByOrgId = async (orgId) => {
     const organization = await OrganizationModel.getOrgByOrgId(orgId);
 
     if(!organization) {
+        logger.warn({ 
+                orgId,
+            }, "Failed to retrieve organization"
+        );
+
         throw new Error("No organization found");
     }
+    logger.info({ 
+            orgId,
+        }, "Organization details retrived"
+    );
 
     return organization;
 }
@@ -67,8 +99,16 @@ const deleteOrganization = async (orgId) => {
     const organization = await OrganizationModel.deleleOrganization(orgId);
 
     if(!organization) {
+        logger.warn({ 
+                orgId,
+            }, "Organization deletion failed"
+        );
         throw new Error("No organization found");
     }
+    logger.info({ 
+            orgId,
+        }, "Organization deleted successfully"
+    );
 
     return organization;
 }
@@ -76,9 +116,17 @@ const deleteOrganization = async (orgId) => {
 const updateOwner = async (orgId, memberId) => {
     const organization = await OrganizationModel.updateOwner(orgId, memberId);
     if(!organization) {
+        logger.warn({ 
+                orgId,
+            }, "Organization owner change failed"
+        );
         throw new Error("Ownership not changed");
     }
-
+    logger.info({ 
+            orgId,
+            newOwner: memberId
+        }, "Organization owner changed"
+    );
     return organization;
 }
 
