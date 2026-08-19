21ca1ec1e8755df09ab4746f1f90ee80f454224a
Alex-Benoy <alexbenoy76@gmail.com>
2026-08-19 23:46:17 +0100

    Added logging to invitations

--- src/services/invitation.service.js
+++ src/services/invitation.service.js
@@ -1,13 +1,28 @@
 import InvitationModel from "../models/invitation.model.js";
 import OrgMemberModel from "../models/orgMember.model.js";
+import logger from "../utils/logger.js";
 
 const newInvitation = async (orgId, email, role) => {
     const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
     const invitation = await InvitationModel.newInvitation(orgId, email, role, expiresAt);
         
     if(!invitation) {
+        logger.warn({
+                organization: orgId,
+                email: email,
+                role: role
+            },
+            "Failed to send invitation to user"
+        );
         throw new Error("invitation not added");
     }
+    logger.info({
+            organization: orgId,
+            email: email,
+            role: role
+        },
+        "Invitation sent to user"
+    );
 
     return invitation;
 }
@@ -16,9 +31,19 @@ const getInvitation = async (token) => {
     const invitation = await InvitationModel.getInvitation(token);
         
     if(!invitation) {
+        logger.warn({
+                token: token
+            },
+            "Failed to retrieve invitation"
+        );
         throw new Error("invitation not found");
     }
 
+    logger.info({
+            token: token
+        },
+        "Retrieved invitation"
+    );
     return invitation;
 }
 
@@ -26,9 +51,19 @@ const getAllInvitationsByOrg = async (orgId) => {
     const invitation = await InvitationModel.getAllInvitationsByOrg(orgId);
         
     if(!invitation) {
+        logger.warn({
+                organization: orgId
+            },
+            "Failed to retrieve invitations"
+        );
         throw new Error("invitation not found");
     }
 
+    logger.info({
+            organization: orgId
+        },
+        "Retrieved invitations"
+    );
     return invitation;
 }
 
@@ -71,6 +106,13 @@ const acceptInvitation = async (token, userId) => {
     const acceptedInvitation =
         await InvitationModel.acceptInvitation(token);
 
+    logger.info({
+            token: token,
+            user: userId,
+            organization: invitation.organization_id
+        },
+        "Accepted invitation"
+    );
     return {
         invitation: acceptedInvitation,
         membership: newMember
@@ -81,9 +123,19 @@ const deleteInvitation = async (invId) => {
     const invitation = await InvitationModel.deleteInvitation(invId);
         
     if(!invitation) {
+        logger.warn({
+                invitation: invId
+            },
+            "Failed to delete invitation"
+        );
         throw new Error("invitation not deleted");
     }
 
+    logger.info({
+            invitation: invId
+        },
+        "Deleted invitation"
+    );
     return invitation;
 }
 
